5dd5f447665087322921014d3f6a92bd026db0e9
alphasuthamedia <alphasuthamedia@gmail.com>
2025-03-04 22:52:24 +0700

    [RED] adding order repository test ~ correct way

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
 
 
 class OrderRepositoryTest {
